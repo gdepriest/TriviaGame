1934102755088ed071597f2c9e11725d39bd19a3
Genevieve DePriest <gdpparton@gmail.com>
2019-05-19 11:07:48 -0600

    I'm satisfied with this to turn in.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,70 +3,79 @@ audioResults.setAttribute("src", "assets/sounds/videoplayback.webm");
 
 
 // Create a list of questions and answers as objects
-var questions = [{
+var questions = [
+    {
     question: "About how long does a turkey egg have to incubate before hatching?",
     answers: ["10 days", "12 days", "28 days", "34 days"],
     correctAnswer: "28 days",
     image: "assets/images/question_1.jpe",
-}, {
+    }, 
+    {
     question: "What is the scientific term for a young turkey that has just hatched?",
     answers: ["baby", "chick", "fawn", "poult"],
     correctAnswer: "poult",
     image: "assets/images/question_2.jpe",
-}, 
-// {
-//     question: "At what age is a turkey hen able to lay her first egg?",
-//     answers: ["5 months", "7 months", "9 months", "12 months"],
-//     correctAnswer: "7 months",
-//     image: "assets/images/question_3.jpe",
-// }, 
-{
+    }, 
+    {
+    question: "At what age is a turkey hen able to lay her first egg?",
+    answers: ["5 months", "7 months", "9 months", "12 months"],
+    correctAnswer: "7 months",
+    image: "assets/images/question_3.jpe",
+    }, 
+    {
     question: "True or False: Turkeys are able to fly.",
     answers: [true, false],
     correctAnswer: true,
     image: "assets/images/question_4.jpe",
-}, 
-// {
-//     question: "Turkeys eat fruits, berries, & seeds.  These items belong in which part of the food chain?",
-//     answers: ["producers", "primary consumers", "secondary consumers", "decomposers"],
-//     correctAnswer: "producers",
-//     image: "assets/images/question_5.jpe",
-// }, {
-//     question: "In a food chain, turkeys eat producers, so they would be classified as ________.",
-//     answers: ["producers", "decomposers", "secondary consumers", "primary consumers"],
-//     correctAnswer: "primary consumers",
-//     image: "assets/images/question_6.jpe",
-// }, {
-//     question: "In a food chain, which of the following would NOT be a secondary consumer of turkeys?",
-//     answers: ["humans", "turtles", "wolves", "coyotes"],
-//     correctAnswer: "turtles",
-//     image: "assets/images/question_7.jpe",
-// },  {
-//     question: "In which category of consumers do turkeys belong?",
-//     answers: ["herbivores", "omnivores", "carnivores",],
-//     correctAnswer: "omnivores",
-//     image: "assets/images/question_8.jpe",
-// },  {
-//     question: "Which would be a correct classification of a turkey?",
-//     answers: ["invertebrate", "marsupial", "vertebrate", "cold-blooded"],
-//     correctAnswer: "vertebrate",
-//     image: "assets/images/question_9.jpe",
-// },  {
-//     question: "Which of the following traits does NOT fit in the 'bird' classification?",
-//     answers: ["vertebrate", "hollow bones", "lay eggs", "produce milk"],
-//     correctAnswer: "produce milk",
-//     image: "assets/images/question_10.jpe",
-// },  {
-//     question: "True or False: Turkeys will also consume insects and small reptiles along wih grass and  berries.",
-//     answers: [true, false],
-//     correctAnswer: true,
-//     image: "assets/images/question_11.jpe",
-// },  {
-//     question: "What is the correct name for adolescent-aged male and female turkeys?",
-//     answers: ["johns & janets", "jakes & jennies", "toms & hens", "bobs & brendas"],
-//     correctAnswer: "jakes & jennies",
-//     image: "assets/images/question_12.jpe",
-// }, 
+    }, 
+    {
+    question: "Turkeys eat fruits, berries, & seeds.  These items belong in which part of the food chain?",
+    answers: ["producers", "primary consumers", "secondary consumers", "decomposers"],
+    correctAnswer: "producers",
+    image: "assets/images/question_5.jpe",
+    }, 
+    {
+    question: "In a food chain, turkeys eat producers, so they would be classified as ________.",
+    answers: ["producers", "decomposers", "secondary consumers", "primary consumers"],
+    correctAnswer: "primary consumers",
+    image: "assets/images/question_6.jpe",
+    }, 
+    {
+    question: "In a food chain, which of the following would NOT be a secondary consumer of turkeys?",
+    answers: ["humans", "turtles", "wolves", "coyotes"],
+    correctAnswer: "turtles",
+    image: "assets/images/question_7.jpe",
+    },  
+    {
+    question: "In which category of consumers do turkeys belong?",
+    answers: ["herbivores", "omnivores", "carnivores",],
+    correctAnswer: "omnivores",
+    image: "assets/images/question_8.jpe",
+    },  
+    {
+    question: "Which would be a correct classification of a turkey?",
+    answers: ["invertebrate", "marsupial", "vertebrate", "cold-blooded"],
+    correctAnswer: "vertebrate",
+    image: "assets/images/question_9.jpe",
+    },  
+    {
+    question: "Which of the following traits does NOT fit in the 'bird' classification?",
+    answers: ["vertebrate", "hollow bones", "lay eggs", "produce milk"],
+    correctAnswer: "produce milk",
+    image: "assets/images/question_10.jpe",
+    },  
+    {
+    question: "True or False: Turkeys will also consume insects and small reptiles along wih grass and  berries.",
+    answers: [true, false],
+    correctAnswer: true,
+    image: "assets/images/question_11.jpe",
+    },  
+    {
+    question: "What is the correct name for adolescent-aged male and female turkeys?",
+    answers: ["johns & janets", "jakes & jennies", "toms & hens", "bobs & brendas"],
+    correctAnswer: "jakes & jennies",
+    image: "assets/images/question_12.jpe",
+    }, 
 ]
 
 
